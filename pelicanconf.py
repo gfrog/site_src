1faf45fee99d72a67b2690c3ba5d833c1f3e1a66
gfrog <gfrog@gfrog.net>
2017-02-08 20:39:45 +0800

    pelican: Add blog site config files.

---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'gfrog'
+SITENAME = 'My Life, My Love!'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["related_posts", "photos"]
+
+THEME = "themes/foundation-default-colours"
+FOUNDATION_FRONT_PAGE_FULL_ARTICLES = False
+FOUNDATION_ALTERNATE_FONTS = False
+FOUNDATION_TAGS_IN_MOBILE_SIDEBAR = False
+FOUNDATION_NEW_ANALYTICS = False
+FOUNDATION_ANALYTICS_DOMAIN = ''
+FOUNDATION_FOOTER_TEXT = '''
+Copyright © 2017 - gfrog.net
+ - Powered by <a href="http://getpelican.com">Pelican</a>
+ and <a href="http://foundation.zurb.com/">Zurb Foundation</a>.
+ Theme by <a href="http://hamaluik.com">Kenton Hamaluik</a>.
+'''
+
+FOUNDATION_PYGMENT_THEME = 'monokai'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+        )
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/gfrog'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
